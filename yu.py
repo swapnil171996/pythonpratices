4502b249d8d7af33d5c44067e088903dffca92c8
swapnil171996 <patilswapnil1103086@gmail.com>
2025-08-10 22:34:41 +0530

    programme added

---
+++ yu.py
@@ -0,0 +1,18 @@
+'''round1="pass"
+round2="pass"
+round3="failed"
+if round1=="pass":
+    print("1st round is cleared")
+    if round2=="pass":
+        print("2nd round is cleared")
+        if round3=="pass":
+           print("congrulation you are placed")
+        else:
+            print("3rd round is failed")
+    else:
+        print("2nd round is failed")
+else:
+    print("1st round is failed")'''
+
+num7 = 90
+result = "even number" if num7%2 == 0 else "odd number"
